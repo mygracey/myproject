804adeec363cfc7d94f4cf3bb693745c637be35b
grace <graceogwe@gmail.com>
2024-10-30 16:41:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+
+  return (
+    <div className="app">
+      <h1>This is a sample react app</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Soluta tempore explicabo deserunt aliquam hic eveniet maxime ipsum corporis veritatis quasi sapiente possimus, autem repudiandae fuga quos odit vel debitis sequi consectetur dolore necessitatibus quo! Minus, ab molestiae fuga tempora ea excepturi, 
+        ullam facilis hic, odio sit eum nobis ratione saepe?</p>
+        <button>Learn more</button>
+    </div>
+  )
+}
+export default App
